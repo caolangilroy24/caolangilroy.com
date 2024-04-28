0ae215e4d38b5786c4284e23f228d1971127adbd
Caolan Gilroy <caolangilroy@gmail.com>
2024-04-28 02:24:31 +0100

    scroll to contact implemented

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,12 +3,18 @@ import '../styles/main/homeBanner.scss'
 
 
 export default function Banner() {
+    const scrollToBottom = () => {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth'
+        });
+    };
   return (
     <div className='banner'>
         <div className='banner-container'>
             <div className='test-grid' >
             </div>
-            <button className='contact-button'>
+            <button className='contact-button' onClick={scrollToBottom}>
               Contact Me
             </button>
         </div>
