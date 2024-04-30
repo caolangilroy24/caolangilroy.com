5ac3e12a2d655ae15aec656bee3ba9019f67c5c8
Caolan Gilroy <caolangilroy@gmail.com>
2024-04-30 16:59:32 +0100

    Setting up for initial Deploy 2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ export default function Contact() {
     <div className='content-content'>
           <div className='contact-content-container'>
             <h1>Lets Work Together</h1>
-            <h3>caolangilroy@gmail.com</h3>
+            <h3>email@gmail.com</h3>
             <div className='github-linkedin-bar'>< img src={github} className="small-icon" alt="github-small" />< img src={linkedin} className="small-icon" alt="linkedin-small" /></div>   
           </div>
     </div>
