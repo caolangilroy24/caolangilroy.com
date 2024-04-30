a16108500052201d8d0273bb12d73ee9ed2a5070
Caolan Gilroy <caolangilroy@gmail.com>
2024-04-30 18:42:24 +0100

    debuging production

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+    base: './',
+    // rollupOptions: {
+    //   output: {
+    //     assetFileNames: 'assets/[name][extname]',
+    //   },
+    // }
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/styles/variables.scss";` // Add a closing backtick here
+        additionalData: `@import "./src/styles/variables.scss";`,
       }
     }
   }
