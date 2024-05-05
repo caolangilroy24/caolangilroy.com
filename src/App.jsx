8226fbf30a6f715fe044d4b6a514383f9c389d58
Caolan Gilroy <caolangilroy@gmail.com>
2024-05-05 23:45:03 +0100

    making progress on responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import logo from '../public/logo.svg'
-// import './styles/main/example.scss'
-// import './styles/main/homeBanner.scss'
-// import './styles/main/contentContainer.scss'
+import './styles/main/example.scss'
+import './styles/main/homeBanner.scss'
+import './styles/main/contentContainer.scss'
 import Home from './components/Home'
 import Contact from './components/Contact'
 import Banner from './components/Banner'
@@ -13,12 +13,25 @@ function App() {
 
   return (
     <>
-    <HighLevelTest/>
+    {/* <HighLevelTest>< img src={logo} className="logo" alt="logo"></img></HighLevelTest>
+     */}  
+    <div class="box__wrapper">
+      < img src={logo} className="logo" alt="logo" />
 
+        <div class="content">
+        <Home/>
+
+        </div>
+        <div class="banner">
+            <div class="banner-container">
+                <div class="photo"></div>
+                <button class="button">click me</button>
+            </div>
+        </div>
+    </div>
       {/* <div className='body-container'>
         < img src={logo} className="logo" alt="logo" />
         <Banner/>
-        <Home/>
         {/* <Contact/> */}
         {/* <Banner/> */}
 
